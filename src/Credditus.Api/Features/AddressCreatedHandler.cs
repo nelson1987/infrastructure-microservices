33dd736581e4d086f058d2a943a097362a749b92
Nelson Neto <nelson.ash@outlook.com>
2023-11-25 21:57:26 +0000

    refactor

--- src/Credditus.Api/Features/AddressCreatedHandler.cs
+++ src/Credditus.Api/Features/AddressCreatedHandler.cs
@@ -1,13 +1,7 @@
 ﻿public class AddressCreatedHandler : IKafkaHandler<string, Address>
 {
-    public void Handle(string key, Address value)
+    public async Task Handle(string key, Address value)
     {
-        if (string.IsNullOrWhiteSpace(key)) throw new ArgumentNullException(nameof(key));
         Console.WriteLine($"Key: {key}, Value: {value.Name}");
-    }
-
-    Task IKafkaHandler<string, Address>.Handle(string key, Address value)
-    {
-        throw new NotImplementedException();
     }
 }
