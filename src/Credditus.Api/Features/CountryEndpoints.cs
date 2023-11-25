e22145f3f5eb5c1e27ffcff8941ba47b02699739
Nelson Neto <nelson.ash@outlook.com>
2023-11-25 17:40:27 -0300

    criacao de test de integracao

--- src/Credditus.Api/Features/CountryEndpoints.cs
+++ src/Credditus.Api/Features/CountryEndpoints.cs
@@ -1,12 +1,15 @@
 namespace AspNetCore8MinimalApis.Endpoints;
-public class Country{}
-public interface ICountryMapper {
-    PagingDto Map(IReadOnlyCollection<Country> countries);
- }
-public interface ICountryService { 
-    Task<IReadOnlyCollection<Country>> GetAllAsync(PagingDto pagging);
+public class Country { }
+public interface ICountryMapper
+{
+    List<Country> Map(List<CountryDto> countries);
+}
+public interface ICountryService
+{
+    Task<List<CountryDto>> GetAllAsync(PagingDto pagging);
 }
 public record PagingDto(int PageIndex, int PageSize);
+public record CountryDto();
 public static class CountryEndpoints
 {
     public static async Task<IResult> GetCountries(
@@ -15,8 +18,8 @@ public static class CountryEndpoints
        ICountryMapper mapper,
        ICountryService countryService)
     {
-        var paging = new PagingDto(pageIndex.HasValue ? pageIndex.Value : 1,
-                                    pageSize.HasValue ? pageSize.Value : 10);
+        var paging = new PagingDto(pageIndex ?? 1,
+                                    pageSize ?? 10);
         var countries = await countryService.GetAllAsync(paging);
         return Results.Ok(mapper.Map(countries));
     }
--- tests/Credittus.UnitTests/GetCountriesTests.cs
+++ tests/Credittus.UnitTests/GetCountriesTests.cs
@@ -1,3 +1,10 @@
+using AspNetCore8MinimalApis.Endpoints;
+using AutoFixture;
+using ExpectedObjects;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using NSubstitute;
+
 namespace Credittus.UnitTests;
 
 public class GetCountriesTests
@@ -12,10 +19,21 @@ public class GetCountriesTests
         _fixture = new Fixture();
     }
     [Fact]
-    public async Task When_GetCountries_Receives_NullPagingParametersAnd_GetAllAsyncMethodReturnsCountries_ShouldFillUpDefaultPagingParametersAndReturnCountries
+    public async Task When_GetCountries_Receives_NullPagingParametersAnd_GetAllAsyncMethodReturnsCountries_ShouldFillUpDefaultPagingParametersAndReturnCountries()
     {
-        //Arrange
-        //Act
-        //Assert
+        // Arrange
+        int? pageIndex = null;
+        int? pageSize = null;
+        var expectedPaging = new PagingDto(PageIndex: 1, PageSize: 10).ToExpectedObject();
+        var countries = _fixture.CreateMany<CountryDto>(2).ToList();
+        var expectedCountries = countries.ToExpectedObject();
+        var mappedCountries = _fixture.CreateMany<Country>(2).ToList();
+        var expectedMappedCountries = mappedCountries.ToExpectedObject();
+        _countryService.GetAllAsync(Arg.Any<PagingDto>()).Returns(x => countries);
+        _countryMapper.Map(Arg.Any<List<CountryDto>>()).Returns(x => mappedCountries);
+        // Act
+        var result = (await CountryEndpoints.GetCountries(pageIndex, pageSize, _countryMapper, _countryService)) as Ok<List<Country>>;
+        // Assert
+        expectedMappedCountries.Should().BeEquivalentTo(result!.Value);
     }
 }
