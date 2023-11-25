78b80d09ca5906fc612971b1013cf4be6f95f1dc
Nelson Neto <nelson.ash@outlook.com>
2023-11-25 20:10:09 +0000

    Inclusao de pacotes no unit tests

--- tests/Credittus.UnitTests/GetCountriesTests.cs
+++ tests/Credittus.UnitTests/GetCountriesTests.cs
@@ -3,14 +3,14 @@ namespace Credittus.UnitTests;
 public class GetCountriesTests
 {
     private readonly ICountryMapper _countryMapper;
- private readonly ICountryService _countryService;
- private readonly Fixture _fixture;
- public GetCountriesTests()
- {
- _countryMapper = Substitute.For<ICountryMapper>();
- _countryService = Substitute.For<ICountryService>();
- _fixture = new Fixture();
- }
+    private readonly ICountryService _countryService;
+    private readonly Fixture _fixture;
+    public GetCountriesTests()
+    {
+        _countryMapper = Substitute.For<ICountryMapper>();
+        _countryService = Substitute.For<ICountryService>();
+        _fixture = new Fixture();
+    }
     [Fact]
     public async Task When_GetCountries_Receives_NullPagingParametersAnd_GetAllAsyncMethodReturnsCountries_ShouldFillUpDefaultPagingParametersAndReturnCountries
     {
